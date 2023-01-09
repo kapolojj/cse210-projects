d99d00e8405bfc59ca4f8a4b0e686c72c30cd247
kapolojj <johanneskapolok@gmail.com>
2023-01-09 20:21:12 +0200

    Addition

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello JJ's World!");
+        int x,y;
+        x=5;
+        y=10;
+        int sum;
+        sum=  x+y;
+        Console.WriteLine(sum);
+        // Console.WriteLine("Hello JJ's World!");
     }
 }
