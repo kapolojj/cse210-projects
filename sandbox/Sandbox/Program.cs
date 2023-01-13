cb857708ca52e3d2169b5df1be2a7c62c906d1a4
kapolojj <johanneskapolok@gmail.com>
2023-01-14 00:17:46 +0200

    update

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,15 +1,68 @@
-using System;
-
-class Program
-{
-    static void Main(string[] args)
+// A code template for the category of things known as Person. The
+    // responsibility of a Person is to hold and display personal information.
+    using System;
+    public class Person
     {
-        int x,y;
-        x=5;
-        y=10;
-        int sum;
-        sum=  x+y;
-        Console.WriteLine(sum);
-        // Console.WriteLine("Hello JJ's World!");
+        // The C# convention is to start member variables with an underscore _
+        public string _givenName = "";
+        public string _familyName = "";
+
+        // A special method, called a constructor that is invoked using the  
+        // new keyword followed by the class name and parentheses.
+        public Person()
+        {
+        }
+
+        // A method that displays the person's full name as used in eastern 
+        // countries or <family name, given name>.
+        public void ShowEasternName()
+        {
+            Console.WriteLine($"{_familyName}, {_givenName}");
+        }
+
+        // A method that displays the person's full name as used in western 
+        // countries or <given name family name>.
+        public void ShowWesternName()
+        {
+            Console.WriteLine($"{_givenName} {_familyName}");
+        }
     }
-}
+
+
+// public class job
+// {
+//     public string  _name = "";
+//     public string _jobs = "";
+//     public job()
+//     {
+//     }
+//     public void ShowMyName ()
+//     {
+//         Console.WriteLine ($"{_name}");
+//     }
+//     public void ShowMyJob ()
+//     {
+//         Console.WriteLine($"{_jobs}");
+//     }
+
+// }
+
+
+
+
+
+// using System;
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         int x,y;
+//         x=5;
+//         y=100;
+//         int sum;
+//         sum=  x+y;
+//         Console.WriteLine(sum);
+//         // Console.WriteLine("Hello JJ's World!");
+//     }
+// }
